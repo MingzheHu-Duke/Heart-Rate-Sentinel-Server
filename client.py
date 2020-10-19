284d0c21fbe1cca24de6a0597a1b86157120b143
Ye Tian <yt149@duke.edu>
2020-10-19 05:51:56 -0400

    new route: get a patient's latest heart rate   (GET)

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests
 
-print("Add new attending with phone number not string")
+print("Add an attending with his phone number is not string")                  # should be 400
 new_attending = {"attending_username": "Everett",
                  "attending_email": "Every@outlook.com",
                  "attending_phone": 9191111110}
@@ -13,5 +13,21 @@ print("Add new patient with str age")
 new_patient = {"patient_id": 101, "attending_username": "Everett",
                "patient_age": "18"}
 r = requests.post("http://127.0.0.1:5000/new_patient", json=new_patient)
+print(r.status_code)
+print(r.text)
+
+
+print("Add an good attending with correct info type")                        # should be 200
+new_attending = {"attending_username": "Trump",
+                 "attending_email": "DTrump@whitehouse.com",
+                 "attending_phone": "100000000"}
+r = requests.post("http://127.0.0.1:5000/new_attending", json=new_attending)
+print(r.status_code)
+print(r.text)
+
+
+print("Get Tom's latest heart rate info")                        # should be 200
+id = "100"
+r = requests.get("http://127.0.0.1:5000/status/" + id)
 print(r.status_code)
 print(r.text)
--- server.py
+++ server.py
@@ -11,6 +11,11 @@ def init_db():
     #   logging.basicConfig(filename="HR_Sentinel.log", filemode='w',
     #                      level=logging.DEBUG)
     add_patient_to_database(100, "Tom", 23)
+    Tom_heart_rate = {"heart_rate": 101,
+                      "status": "tachycardic",
+                      "timestamp": "2018-03-09 11:00:36"}
+    patient_db[0]["heart_rate_history"].append(Tom_heart_rate)
+    print(patient_db)
     add_attending_to_database("Tom", "tom@gmail.com", "919-865-5674")
 
 
@@ -22,6 +27,7 @@ def add_patient_to_database(patient_id=None, attending_username=None,
                    "heart_rate_history": list()}
     patient_db.append(new_patient)
     # logging.info("Added patient {}".format(name))
+    print("patient database:\r")
     print(patient_db)
 
 
@@ -32,7 +38,17 @@ def add_attending_to_database(attending_username, attending_email=None,
                      "attending_phone": attending_phone}
     attending_db.append(new_attending)
     # logging.info("Added patient {}".format(name))
+    print("attending database:\r")
     print(attending_db)
+
+
+def validate_post_input(in_data, expected_key, expected_types):
+    for key, v_type in zip(expected_key, expected_types):
+        if key not in in_data.keys():
+            return "{} key not found in input".format(key)
+        if type(in_data[key]) != v_type:
+            return "{} key value has wrong variable type".format(key)
+    return True
 
 
 @app.route("/new_patient", methods=["POST"])
@@ -91,31 +107,17 @@ def process_new_attending(in_data):
     expected_key = ["attending_username", "attending_email", "attending_phone"]
     expected_types = [str, str, str]
     validate_input = validate_post_input(in_data, expected_key, expected_types)
-    parse_phone(in_data, validate_input)
+    if validate_input is not True:
+        return validate_input + ", please make sure all your info are " \
+                            "in the type of string!", 400
     info_valid = attending_info_detect(in_data)
     if info_valid is not True:
-        return info_valid, 400
+        return info_valid + "please make sure all your info are " \
+                            "in the type of string!", 400
     add_attending_to_database(attending_username=in_data["attending_username"],
                               attending_email=in_data["attending_email"],
                               attending_phone=in_data["attending_phone"])
     return "Attending successfully added", 200
-
-
-def parse_phone(in_data, validate_input):
-    if validate_input is not True:
-        if type(in_data["attending_phone"]) == int:
-            in_data["attending_phone"] = str(in_data["attending_phone"])
-        else:
-            return validate_input, 400
-
-
-def validate_post_input(in_data, expected_key, expected_types):
-    for key, v_type in zip(expected_key, expected_types):
-        if key not in in_data.keys():
-            return "{} key not found in input".format(key)
-        if type(in_data[key]) != v_type:
-            return "{} key value has wrong variable type".format(key)
-    return True
 
 
 def attending_info_detect(in_data):
@@ -126,9 +128,55 @@ def attending_info_detect(in_data):
 
 
 # @app.route("/heart_rate", methods=["POST"]) #Mingzhe
+#这里注意，保存heart_rate一定要保存成int, 无论input是str 还是 int
+#然后保存的时候每一次heart_rate是一个单独的dictionary，这个dictionary至少要有以下三个key+value pair
+#                       {"heart_rate": 101,
+#                      "status": "tachycardic" | "not tachycardic",
+ #                     "timestamp": "2018-03-09 11:00:36"}
+#这样我后面才能用
+
+@app.route("/status/<patient_id>", methods=["GET"]) #YT
+def get_get_results(patient_id):
+    answer, server_status = get_test(patient_id)
+    # return results
+    return answer, server_status
 
 
-# @app.route("/status/<patient_id>", methods=["GET"]) #YT
+def get_test(patient_id):
+    int_id = id_is_int(patient_id)
+    if int_id is True:
+        patient = find_id(patient_id)
+        if patient is False:
+            return "Could not find a matched patient in database", 400
+        have_latest_hr = latest_hr(patient)
+        if have_latest_hr == False:
+           return "This patient doesn't have any heart rate history", 400
+        return jsonify(have_latest_hr), 200
+    return "Please use an integer or a string of an integer containing an ID number", 400
+
+
+def id_is_int(patient_id):
+    try:
+        type(patient_id) in [str, int]
+        return True
+    except ValueError:
+        return False
+
+
+def find_id(patient_id):
+    for patient in patient_db:
+        if patient["patient_id"] == int(patient_id):
+            return patient
+        return False
+
+
+def latest_hr(patient):
+    if len(patient["heart_rate_history"]) == 0:
+        return False
+    latest_heart_rate = {"heart_rate": patient["heart_rate_history"][-1]["heart_rate"],
+                         "status": patient["heart_rate_history"][-1]["status"],
+                         "timestamp": patient["heart_rate_history"][-1]["timestamp"]}
+    return latest_heart_rate
 
 
 # @app.route("/heart_rate/<patient_id>", methods=["GET"]) #Mingzhe
