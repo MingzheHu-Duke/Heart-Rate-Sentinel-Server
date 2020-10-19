75fbc6ad3b17a49ecf57ea695c52ec1ab6d10dd0
MingzheHu-Duke <mingzhehu511@gmail.com>
2020-10-18 23:22:01 -0400

    add patient route

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 import requests
 
-print("Add new attending with good inputs")
-new_attending = {"attending_username": "Everett", "attending_email": "Every@outlook.com", "attending_phone": 919111111}
+print("Add new attending with phone number not string")
+new_attending = {"attending_username": "Everett",
+                 "attending_email": "Every@outlook.com",
+                 "attending_phone": 9191111110}
 r = requests.post("http://127.0.0.1:5000/new_attending", json=new_attending)
+print(r.status_code)
+print(r.text)
+
+
+print("Add new patient with str age")
+new_patient = {"patient_id": 101, "attending_username": "Everett",
+               "patient_age": "18"}
+r = requests.post("http://127.0.0.1:5000/new_patient", json=new_patient)
 print(r.status_code)
 print(r.text)
